9f417e445dff2d396e77823c6d187ffe7d00d045
Tatyana404 <kot.kate2020@gmail.com>
2022-01-08 21:25:22 +0200

    Fix scroll

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -4,23 +4,25 @@ const display = document.querySelector('.display')
 const buttons = document.querySelectorAll('button')
 const date = document.querySelector('.date')
 
-const reset = () => (display.value = '')
-
-const press = x => (display.value += x)
-
-const calc = () => (display.value = eval(display.value))
-
-buttonReset.addEventListener('click', () => reset())
-
-buttonCalc.addEventListener('click', () => calc())
-
 display.addEventListener('keydown', event => event.preventDefault())
+
+buttonReset.addEventListener('click', () => (display.value = ''))
+
+buttonCalc.addEventListener('click', event => {
+  if (event && display.value === '') {
+    return
+  }
+  display.value = eval(display.value)
+})
 
 buttons.forEach(button => {
   if (button.innerHTML === 'C' || button.innerHTML === '=') {
     return
   }
-  button.addEventListener('click', () => press(button.innerHTML))
+  button.addEventListener('click', () => {
+    display.value += button.innerHTML
+    display.scrollTop = display.scrollHeight
+  })
 })
 
 setInterval(
