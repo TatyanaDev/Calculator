baa062dad9ebafc5402ef934afd7c3ec32107f3e
Tatyana Karpenko <kot.kate2020@gmail.com>
2025-11-26 15:50:36 +0100

    rename date to time and improve JS

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -1,35 +1,37 @@
-const buttonReset = document.querySelector('.reset')
-const buttonCalc = document.querySelector('.calc')
-const display = document.querySelector('.display')
-const buttons = document.querySelectorAll('button')
-const date = document.querySelector('.date')
-
-display.addEventListener('keydown', event => event.preventDefault())
-
-buttonReset.addEventListener('click', () => (display.value = ''))
-
-buttonCalc.addEventListener('click', event => {
-  if (event && display.value === '') {
-    return
-  }
-  display.value = eval(display.value)
-})
-
-buttons.forEach(button => {
-  if (button.innerHTML === 'C' || button.innerHTML === '=') {
-    return
-  }
-  button.addEventListener('click', () => {
-    display.value += button.innerHTML
-    display.scrollTop = display.scrollHeight
+document.addEventListener("DOMContentLoaded", () => {
+  const buttonReset = document.querySelector('.reset')
+  const buttonCalc = document.querySelector('.calc')
+  const display = document.querySelector('.display')
+  const buttons = document.querySelectorAll('button')
+  const time = document.querySelector('.time')
+  
+  display.addEventListener('keydown', event => event.preventDefault())
+  
+  buttonReset.addEventListener('click', () => (display.value = ''))
+  
+  buttonCalc.addEventListener('click', event => {
+    if (event && display.value === '') {
+      return
+    }
+    display.value = eval(display.value)
   })
-})
-
-setInterval(
-  () =>
-    (date.innerHTML = new Date().toLocaleString('ru', {
-      hour: 'numeric',
-      minute: 'numeric'
-    })),
-  1000
-)
+  
+  buttons.forEach(button => {
+    if (button.innerHTML === 'C' || button.innerHTML === '=') {
+      return
+    }
+    button.addEventListener('click', () => {
+      display.value += button.innerHTML
+      display.scrollTop = display.scrollHeight
+    })
+  })
+  
+  setInterval(
+    () =>
+      (time.innerHTML = new Date().toLocaleString('ru', {
+        hour: 'numeric',
+        minute: 'numeric'
+      })),
+    1000
+  )
+});
